f5bbd358fe33083708ac4f6bf478575702dd380c
Tim Rekelj <hello@timrekelj.si>
2024-09-29 14:08:46 +0200

    Add parser and parser test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::Read;
 use std::process;
 
 #[derive(Debug)]
+#[derive(Clone)]
 #[derive(PartialEq)]
 enum Lexem {
     NextCell,
@@ -16,6 +17,18 @@ enum Lexem {
     EndLoop
 }
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+enum Instruction {
+    NextCell,
+    PreviousCell,
+    Increment,
+    Decrement,
+    Write,
+    Read,
+    Loop(Vec<Instruction>)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -26,9 +39,9 @@ fn main() {
 
     let filename: &str = &args[1];
 
-    let mut file: File = File::open(filename).expect("File not found.");
+    let mut file: File = File::open(filename).expect("File not found");
     let mut file_content: String = String::new();
-    file.read_to_string(&mut file_content).expect("There was an error reading the file.");
+    file.read_to_string(&mut file_content).expect("There was an error reading the file");
 
     let _: Vec<Lexem> = lexer(file_content);
 }
@@ -47,11 +60,55 @@ fn lexer(file_content: String) -> Vec<Lexem> {
             ',' => lexems.push(Lexem::Read),
             '[' => lexems.push(Lexem::StartLoop),
             ']' => lexems.push(Lexem::EndLoop),
-            _ => { /* everything else is skipped */}
+            _ => { /* everything else is skipped */ }
         }
     }
 
     return lexems;
+}
+
+fn parser(lexems: Vec<Lexem>) -> Vec<Instruction> {
+    let mut instructions: Vec<Instruction> = Vec::new();
+    let mut loop_stack: usize = 0;
+    let mut loop_start: usize = 0;
+
+    for (i, lexem) in lexems.iter().enumerate() {
+        if loop_stack == 0 {
+            match lexem {
+                Lexem::NextCell => instructions.push(Instruction::NextCell),
+                Lexem::PreviousCell => instructions.push(Instruction::PreviousCell),
+                Lexem::Increment => instructions.push(Instruction::Increment),
+                Lexem::Decrement => instructions.push(Instruction::Decrement),
+                Lexem::Write => instructions.push(Instruction::Write),
+                Lexem::Read => instructions.push(Instruction::Read),
+                Lexem::StartLoop => {
+                    loop_start = i;
+                    loop_stack += 1;
+                }
+                Lexem::EndLoop => panic!("Loop ended without start")
+            }
+        } else {
+            match lexem {
+                Lexem::StartLoop => {
+                    loop_stack += 1;
+                }
+                Lexem::EndLoop => {
+                    loop_stack -= 1;
+
+                    if loop_stack == 0 {
+                        instructions.push(Instruction::Loop(parser(lexems[loop_start+1..i].to_vec())));
+                    }
+                }
+                _ => { /* this should never happen */ }
+            }
+        }
+    }
+
+    if loop_stack != 0 {
+        panic!("Loop started and never ended")
+    }
+
+    return instructions;
 }
 
 #[test]
@@ -73,4 +130,30 @@ fn lexer_test() {
         ]
     );
     assert_eq!(lexems.len(), 8);
+}
+
+#[test]
+fn parser_test() {
+    let content: String = String::from("Text should not be counted ><+-.,[[]>,<]");
+    let lexems: Vec<Lexem> = lexer(content);
+    let instructions: Vec<Instruction> = parser(lexems);
+
+    assert_eq!(
+        instructions,
+        [
+            Instruction::NextCell,
+            Instruction::PreviousCell,
+            Instruction::Increment,
+            Instruction::Decrement,
+            Instruction::Write,
+            Instruction::Read,
+            Instruction::Loop(Vec::from([
+                Instruction::Loop(Vec::new()),
+                Instruction::NextCell,
+                Instruction::Read,
+                Instruction::PreviousCell
+            ])),
+        ]
+    );
+    assert_eq!(instructions.len(), 7);
 }
